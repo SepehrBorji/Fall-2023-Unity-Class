169129b64975736aaf208980ab0c36155f7c4239
Sepehr Borji <Sepehr_borji@hotmail.com>
2023-11-01 21:49:42 -0700

    Week 5 - Fixed Inventory UI issue, added click feature (doesn't work with our game, but applicable to your own projects)

--- fall 2023 SD43/Assets/Scripts/InventoryManager.cs
+++ fall 2023 SD43/Assets/Scripts/InventoryManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
     [HideInInspector] public List<Item> inventory = new List<Item>();
     public GameObject inventoryItemPrefab;
     public Transform toolbarTransform;
+    private InventorySlot inventorySlotScript;
 
     // Start is called before the first frame update
     void Start()
@@ -47,11 +49,21 @@ public class InventoryManager : MonoBehaviour
 
     private void ListItems()
     {
+        // clear the inventory UI
+        foreach (Transform item in toolbarTransform)
+        {
+            Destroy(item.gameObject);
+        }
+
+        // display all items in Inventory onto the UI
         foreach (Item item in inventory)
         {
             GameObject obj = Instantiate(inventoryItemPrefab, toolbarTransform);
 
-            Text itemNameText = obj.GetComponentInChildren<Text>();
+            inventorySlotScript = obj.GetComponent<InventorySlot>();
+            inventorySlotScript.currentItem = item;
+
+            TextMeshProUGUI itemNameText = obj.GetComponentInChildren<TextMeshProUGUI>();
             itemNameText.text = item.itemName;
 
             Image itemIconImage = obj.GetComponent<Image>();
