71b0ba978f7eb90d1af1071a7da1e991f4274a87
Sepehr Borji <Sepehr_borji@hotmail.com>
2023-12-13 21:16:20 -0800

    Week 10 - Finished with data persistence JSON

--- Data-persistence-sample/Assets/Scripts/MainManager.cs
+++ Data-persistence-sample/Assets/Scripts/MainManager.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
     public static MainManager Instance;
     public Color TeamColor;
+
+    public void SaveColor()
+    {
+        SaveData colorData = new SaveData();
+        colorData.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(colorData);
+        Debug.Log("Data written: " + json);
+
+        File.WriteAllText(Application.persistentDataPath + "/colorSaveData.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string filePath = Application.persistentDataPath + "/colorSaveData.json";
+
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+
+            Debug.Log("Data read: " + json);
+
+            SaveData colorData = JsonUtility.FromJson<SaveData>(json);
+            TeamColor = colorData.TeamColor;
+        }
+    }
 
     void Awake()
     {
@@ -20,13 +53,11 @@ public class MainManager : MonoBehaviour
 
         // make sure data persists
         DontDestroyOnLoad(gameObject);
+
+        LoadColor();
     }
 
 }
 
-[System.Serializable]
-class SaveData
-{
-    public Color TeamColor;
-}
+
 
